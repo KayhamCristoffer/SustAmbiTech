17462159cbe46c0770fb9c1de3a90e762c3c0999
KayhamCristoffer <kayham98.@hotmail.com>
2025-09-21 10:22:33 -0300

    update

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,5 @@
 const firebaseConfig = {
-//  "apiKey": "AIzaSyBMg0-VczwxYbvmv0-kLNHDId1z2qep6hk",
-//  "authDomain": "bd-sustambitech.firebaseapp.com",
-//  "databaseURL": "https://bd-sustambitech-default-rtdb.firebaseio.com",
-//  "projectId": "bd-sustambitech",
-//  "storageBucket": "bd-sustambitech.firebasestorage.app",
-//  "messagingSenderId": "888937636189",
-//  "appId": "1:888937636189:web:48c0c2a858678b017fc8b4",
-//  "measurementId": "G-Q9KW0L9C76"
+
   apiKey: "AIzaSyBMg0-VczwxYbvmv0-kLNHDId1z2qep6hk",
   authDomain: "bd-sustambitech.firebaseapp.com",
   databaseURL: "https://bd-sustambitech-default-rtdb.firebaseio.com",
@@ -18,11 +11,3 @@ const firebaseConfig = {
 }
 
 export { firebaseConfig };
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
